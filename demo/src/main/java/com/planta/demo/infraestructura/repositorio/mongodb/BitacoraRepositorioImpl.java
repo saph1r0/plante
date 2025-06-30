7df7a76d5cef8e9d25dd9fea4aab3ee6d510b3e4
saph1r0 <sophiqwe123@gmail.com>
2025-06-30 13:13:40 -0500

    repbit

--- demo/src/main/java/com/planta/demo/infraestructura/repositorio/mongodb/BitacoraRepositorioImpl.java
+++ demo/src/main/java/com/planta/demo/infraestructura/repositorio/mongodb/BitacoraRepositorioImpl.java
@@ -1,236 +1,82 @@
-package repositorio.mongodb;
+package com.planta.demo.infraestructura.repositorio.mongodb;
 
-import dominio.modelo.IBitacoraRepositorio;
-import dominio.modelo.bitacora.Bitacora;
+import com.planta.demo.dominio.modelo.bitacora.Bitacora;
+import com.planta.demo.dominio.modelo.IBitacoraRepositorio;
 
-import java.io.*;
-import java.util.*;
+import java.util.Date;
+import java.util.List;
 
-/**
- * 
- */
-public class BItacoraRepositorioIMpl implements IBitacoraRepositorio {
+public class BitacoraRepositorioImpl implements IBitacoraRepositorio {
 
-    /**
-     * Default constructor
-     */
-    public BItacoraRepositorioIMpl() {
+    public BitacoraRepositorioImpl() {
     }
 
-    /**
-     * @param bitacora 
-     * @return
-     */
+    @Override
     public void guardar(Bitacora bitacora) {
-        // TODO implement here
-        return null;
+        // TODO: guardar en MongoDB
     }
 
-    /**
-     * @param id 
-     * @return
-     */
+    @Override
     public Bitacora obtenerPorId(String id) {
-        // TODO implement here
+        // TODO: buscar por ID
         return null;
     }
 
-    /**
-     * @return
-     */
+    @Override
     public List<Bitacora> listarTodas() {
-        // TODO implement here
+        // TODO
         return null;
     }
 
-    /**
-     * @param id 
-     * @return
-     */
+    @Override
     public void eliminar(String id) {
-        // TODO implement here
-        return null;
+        // TODO
     }
 
-    /**
-     * @param bitacora 
-     * @return
-     */
+    @Override
     public void actualizar(Bitacora bitacora) {
-        // TODO implement here
-        return null;
+        // TODO
     }
 
-    /**
-     * @param plantaId 
-     * @return
-     */
+    @Override
     public List<Bitacora> listarPorPlanta(String plantaId) {
-        // TODO implement here
+        // TODO
         return null;
     }
 
-    /**
-     * @param usuarioId 
-     * @return
-     */
+    @Override
     public List<Bitacora> listarPorUsuario(String usuarioId) {
-        // TODO implement here
+        // TODO
         return null;
     }
 
-    /**
-     * @param fechaInicio 
-     * @param fechaFin 
-     * @return
-     */
+    @Override
     public List<Bitacora> listarPorFecha(Date fechaInicio, Date fechaFin) {
-        // TODO implement here
+        // TODO
         return null;
     }
 
-    /**
-     * @param tipoActividad 
-     * @return
-     */
+    @Override
     public List<Bitacora> listarPorTipoActividad(String tipoActividad) {
-        // TODO implement here
+        // TODO
         return null;
     }
 
-    /**
-     * @param descripcion 
-     * @return
-     */
+    @Override
     public List<Bitacora> buscarPorDescripcion(String descripcion) {
-        // TODO implement here
+        // TODO
         return null;
     }
 
-    /**
-     * @param plantaId 
-     * @return
-     */
+    @Override
     public Long contarRegistrosPorPlanta(String plantaId) {
-        // TODO implement here
+        // TODO
         return null;
     }
 
-    /**
-     * @param plantaId 
-     * @return
-     */
+    @Override
     public Bitacora obtenerUltimoRegistro(String plantaId) {
-        // TODO implement here
+        // TODO
         return null;
     }
-
-    /**
-     * @param bitacora 
-     * @return
-     */
-    public void guardar(Bitacora bitacora) {
-        // TODO implement IBitacoraRepositorio.guardar() here
-        return null;
-    }
-
-    /**
-     * @param id 
-     * @return
-     */
-    public Bitacora obtenerPorId(String id) {
-        // TODO implement IBitacoraRepositorio.obtenerPorId() here
-        return null;
-    }
-
-    /**
-     * @return
-     */
-    public List<Bitacora> listarTodas() {
-        // TODO implement IBitacoraRepositorio.listarTodas() here
-        return null;
-    }
-
-    /**
-     * @param id 
-     * @return
-     */
-    public void eliminar(String id) {
-        // TODO implement IBitacoraRepositorio.eliminar() here
-        return null;
-    }
-
-    /**
-     * @param bitacora 
-     * @return
-     */
-    public void actualizar(Bitacora bitacora) {
-        // TODO implement IBitacoraRepositorio.actualizar() here
-        return null;
-    }
-
-    /**
-     * @param plantaId 
-     * @return
-     */
-    public List<Bitacora> listarPorPlanta(String plantaId) {
-        // TODO implement IBitacoraRepositorio.listarPorPlanta() here
-        return null;
-    }
-
-    /**
-     * @param usuarioId 
-     * @return
-     */
-    public List<Bitacora> listarPorUsuario(String usuarioId) {
-        // TODO implement IBitacoraRepositorio.listarPorUsuario() here
-        return null;
-    }
-
-    /**
-     * @param fechaInicio 
-     * @param fechaFin 
-     * @return
-     */
-    public List<Bitacora> listarPorFecha(Date fechaInicio, Date fechaFin) {
-        // TODO implement IBitacoraRepositorio.listarPorFecha() here
-        return null;
-    }
-
-    /**
-     * @param tipoActividad 
-     * @return
-     */
-    public List<Bitacora> listarPorTipoActividad(String tipoActividad) {
-        // TODO implement IBitacoraRepositorio.listarPorTipoActividad() here
-        return null;
-    }
-
-    /**
-     * @param descripcion 
-     * @return
-     */
-    public List<Bitacora> buscarPorDescripcion(String descripcion) {
-        // TODO implement IBitacoraRepositorio.buscarPorDescripcion() here
-        return null;
-    }
-
-    /**
-     * @param plantaId 
-     * @return
-     */
-    public Long contarRegistrosPorPlanta(String plantaId) {
-        // TODO implement IBitacoraRepositorio.contarRegistrosPorPlanta() here
-        return null;
-    }
-
-    /**
-     * @param plantaId 
-     * @return
-     */
-    public Bitacora obtenerUltimoRegistro(String plantaId) {
-        // TODO implement IBitacoraRepositorio.obtenerUltimoRegistro() here
-        return null;
-    }
-
 }
