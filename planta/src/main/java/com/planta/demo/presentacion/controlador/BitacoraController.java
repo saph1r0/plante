88cac0ff183206ee485f8f21db966cffc4ddf11a
AN5 <86611820+AngelaSoto05@users.noreply.github.com>
2025-07-09 17:25:38 -0500

    :Subiendo errores

--- planta/src/main/java/com/planta/demo/presentacion/controlador/BitacoraController.java
+++ planta/src/main/java/com/planta/demo/presentacion/controlador/BitacoraController.java
@@ -14,6 +14,7 @@ public class BitacoraController {
      * Default constructor
      */
     public BitacoraController() {
+        this.bitacoraService = new BitacoraService();
     }
 
     /**
@@ -26,8 +27,17 @@ public class BitacoraController {
      * @return
      */
     public List<Bitacora> obtenerPorPlanta(Long plantaId) {
-        // TODO implement here
-        return null;
+        // Ejemplo: obtienes todos y filtras por plantaId
+        List<Bitacora> todas = bitacoraRepository.findAll();
+        List<Bitacora> filtradas = new ArrayList<>();
+
+        for (Bitacora b : todas) {
+            if (b.getPlantaId().equals(plantaId)) {
+                filtradas.add(b);
+            }
+        }
+
+        return filtradas;
     }
 
     /**
